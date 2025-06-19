123192bb84d9e8f1fb0855cd0356d497695d4ab6
Sharad Banga <sharadiit03@gmail.com>
2025-06-19 22:29:01 +0530

    adding collections

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function App() {
       
       <div className={`${isDark?"dark":""}`}>
 
-          <div  className="h-[250vh] w-[100%] flex justify-center  dark:bg-[url('images/gradi2.png')]  bg-[url('/images/gradi1.png')] bg-no-repeat bg-cover bg-fixed transition-all duration-500" >
+          <div  className="h-[250vh] w-[100%] flex justify-center  dark:bg-black  bg-[url('/images/gradi1.png')] bg-no-repeat bg-cover bg-fixed transition-all duration-500" >
             <div className="w-[100%] md:w-[50%] h-full ">
 
               <Navbar darki={isDark} toggleDark={toggleDarkMode} />
--- src/comp/ProjectPage.tsx
+++ src/comp/ProjectPage.tsx
@@ -1,6 +1,19 @@
+import { useEffect } from "react";
 import Projects from "./Projects";
 
 export default function ProjectPage(){
+
+  
+
+useEffect(()=>{
+  
+    window.scrollTo(0,0);
+  },[])
+
+
+
+
+
   return(
     <div className="mt-20">
       <Projects/>
